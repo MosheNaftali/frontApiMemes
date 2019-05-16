76e0305fddbc9a122c03f21081eee979524be64b
Moshe Shabes <moshnaf@gmail.com>
2019-05-16 18:10:12 -0500

    Primer Commit

---
+++ build/precache-manifest.c5f8c605ccad1ddeaff8ca60773272de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fbb4554ff20970006ab608eff0d6096",
+    "url": "/static/media/logo.0fbb4554.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a23b304ead86ccbf693d",
+    "url": "/static/js/main.a23b304e.chunk.js"
+  },
+  {
+    "revision": "861c84428e2fb6145ba9",
+    "url": "/static/js/2.861c8442.chunk.js"
+  },
+  {
+    "revision": "a23b304ead86ccbf693d",
+    "url": "/static/css/main.1dcf7a04.chunk.css"
+  },
+  {
+    "revision": "e2389e5bd9acfc41a7c34452dc51fbf5",
+    "url": "/index.html"
+  }
+];
